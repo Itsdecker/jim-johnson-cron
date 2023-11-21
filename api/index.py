d48d315c8d2d552b1a7a888e692bf7dcd225c28c
Tammitha <Infodesk@torquebdc.com>
2023-11-20 22:10:23 -0500

    pdfs

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from api.utils.download_drive_ep import download_drive_ep
+from api.utils.pdf_csv import convert_pdfs_csv
 
 
 app = FastAPI()
 
 
-@app.get("/api/download_drive_ep")
-def trigger_download():
-    result = download_drive_ep()
+@app.get("/api/convert-pdfs")
+def pdf_csv():
+    result = convert_pdfs_csv()
     return result
